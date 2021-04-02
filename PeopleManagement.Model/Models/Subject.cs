53e20b126d1e78647a0cf00041fc2c47561e1252
Binh Le <binhle@b13technology.com>
2021-04-02 19:59:01 +0700

    Add PeopleManagement.Model Class library project (This library is where we ‘ll keep all of our domain objects)

---
+++ PeopleManagement.Model/Models/Subject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeopleManagement.Model.Models
+{
+    public class Subject
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string SubjectName { get; set; }
+        public int SubjectValue { get; set; }
+    }
+}
---
+++ PeopleManagement.Model/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeopleManagement.Model.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public int NRIC { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public decimal Birthday { get; set; }
+        public string AvaiableDate { get; set; }
+        public virtual IEnumerable<Subject> Subjects { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public User()
+        {
+            DateCreated = DateTime.UtcNow;
+        }
+    }
+}
